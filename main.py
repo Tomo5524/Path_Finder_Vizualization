10995c4286ea5602886595434fa72084812dd1ff
Tomo5524 <ttoommoosrst@gmail.com>
2020-04-14 23:25:03 +0900

    commit daily

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-
-try:
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
-except:
-    import install_requirements  # install packages
-
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
-
+import pygame
+import sys
+import math
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import os
+import timeit
+import time
+import heapq
 
 WIDTH = 800
 HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill((255,255,255))
+screen.fill((255, 255, 255))
 
 
 class spot:
@@ -32,113 +22,53 @@ class spot:
         self.f = 0
         self.g = 0
         self.h = 0
-        self.neighbors = []
+        self.neighbors = set()
         self.previous = None
         self.block = False
         self.closed = False
+        self.opened = False
         self.value = 1
+        self.visited = False
 
     def draw_line(self, color, st):
         # create cases for 8 directions
-        pre_loc = grid[self.i][self.j].previous
-        r = pre_loc.i * (WIDTH // row)
-        c =  pre_loc.j * (HEIGHT // cols)
-        #r,c = self.i,self.j
-        cur_loc_x = self.i * (WIDTH // row)
-        cur_loc_y = self.j * (HEIGHT // cols)
+        if self.previous:
+            pre_loc = grid[self.i][self.j].previous
+            r = pre_loc.i * (WIDTH // row)
+            c = pre_loc.j * (HEIGHT // cols)
+            # r,c = self.i,self.j
+            cur_loc_x = self.i * (WIDTH // row)
+            cur_loc_y = self.j * (HEIGHT // cols)
 
-        pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + (HEIGHT // cols) // 2),
-                         (r + ((WIDTH // row) // 2),  c + (HEIGHT // cols) // 2), st)
+            pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + (HEIGHT // cols) // 2),
+                             (r + ((WIDTH // row) // 2), c + (HEIGHT // cols) // 2), st)
 
-        # end line goes into pre_loc
-        # cur_locx and cur_loc_y denotes the topleft of the current grid
-        # if pre_loc == grid[r - 1][c -1]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x - ((WIDTH // row) // 2), cur_loc_y - ((HEIGHT // cols) // 2)), st)
-        #
-        # elif pre_loc == grid[r + 1][c]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x + (WIDTH // row), cur_loc_y + (HEIGHT // cols)), st)
-        #
-        # elif pre_loc == grid[r - 1][c]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x - (WIDTH // row), cur_loc_y + ((HEIGHT // cols) // 2)), st)
-        #
-        # elif pre_loc == grid[r + 1][c]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x + (WIDTH // row), cur_loc_y), st)
-        #
-        # elif pre_loc == grid[r][c - 1]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y - (HEIGHT // cols)), st)
-        #
-        # elif pre_loc == grid[r][c + 1]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x, cur_loc_y + (HEIGHT // cols)), st)
-        #
-        # elif pre_loc == grid[r + 1][c - 1]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x + (WIDTH // row), cur_loc_y - ((HEIGHT // cols) // 2)), st)
-        #
-        # elif pre_loc == grid[r - 1][c + 1]:
-        #     pygame.draw.line(screen, color, (cur_loc_x + ((WIDTH // row) // 2), cur_loc_y + ((HEIGHT // cols) // 2)),
-        #                      (cur_loc_x - ((WIDTH // row) // 2), cur_loc_y + (HEIGHT // cols)), st)
-
-        # if i > 0 and grid[self.i - 1][j].block == False:
-        #     self.neighbors.append(grid[self.i - 1][j])
-        # if j < row - 1 and grid[self.i][j + 1].block == False:
-        #     self.neighbors.append(grid[self.i][j + 1])
-        # if j > 0 and grid[self.i][j - 1].block == False:
-        #     self.neighbors.append(grid[self.i][j - 1])
-        #
-        # # if there are only 4 neighbors, it is manhattan distance
-        # # if there are 8, then that is diagonal
-        # if j > 0 and i > 0 and grid[self.i-1][j - 1].block == False:
-        #     self.neighbors.append(grid[self.i-1][j - 1])
-        # if j < row -1 and i < cols-1 and grid[self.i+1][j + 1].block == False:
-        #     self.neighbors.append(grid[self.i+1][j + 1])
-        # if j > 0 and i < cols-1 and grid[self.i+1][j - 1].block == False:
-        #     self.neighbors.append(grid[self.i+1][j - 1])
-        # if j < row -1 and i > 0 and grid[self.i-1 ][j + 1].block == False:
-        #     self.neighbors.append(grid[self.i-1][j + 1])
-
-        # cur_loc_x = self.i * (WIDTH//row)
-        # cur_loc_y = self.j * (HEIGHT//cols)
-
-        # else:
-        #     pygame.draw.line(screen, color, (cur_loc_x, cur_loc_y),(cur_loc_x + (WIDTH//row), cur_loc_y + (HEIGHT//cols)), st)
-
-        pygame.display.update()
-
-    def show(self, color, st):
-        if self.closed == False:
-            pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), st)
             pygame.display.update()
 
+    def show(self, color, size):
 
-    def addNeighbors(self, grid):
-        i = self.i
-        j = self.j
-        if i < cols - 1 and grid[self.i + 1][j].block == False:
-            self.neighbors.append(grid[self.i + 1][j])
-        if i > 0 and grid[self.i - 1][j].block == False:
-            self.neighbors.append(grid[self.i - 1][j])
-        if j < row - 1 and grid[self.i][j + 1].block == False:
-            self.neighbors.append(grid[self.i][j + 1])
-        if j > 0 and grid[self.i][j - 1].block == False:
-            self.neighbors.append(grid[self.i][j - 1])
+        pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), size)
+        pygame.display.update()
 
-        # if there are only 4 neighbors, it is manhattan distance
-        # if there are 8, then that is diagonal
-        if j > 0 and i > 0 and grid[self.i-1][j - 1].block == False:
-            self.neighbors.append(grid[self.i-1][j - 1])
-        if j < row -1 and i < cols-1 and grid[self.i+1][j + 1].block == False:
-            self.neighbors.append(grid[self.i+1][j + 1])
-        if j > 0 and i < cols-1 and grid[self.i+1][j - 1].block == False:
-            self.neighbors.append(grid[self.i+1][j - 1])
-        if j < row -1 and i > 0 and grid[self.i-1 ][j + 1].block == False:
-            self.neighbors.append(grid[self.i-1][j + 1])
+    def show_open_and_closed(self, color, size):
+        # plus and minus 1 to show margin
+        pygame.draw.rect(screen, color, (self.i * w, self.j * h, w - 1, h - 1), size)
+        pygame.display.update()
 
+    def is_diagonally_walkable(self, x, y):
+        if (self.i, self.j) == (x - 1, y + 1) and grid[x - 1][y].block and grid[x][y + 1].block:
+            return True
+
+        elif (self.i, self.j) == (x + 1, y + 1) and grid[x + 1][y].block and grid[x][y + 1].block:
+            return True
+
+        elif (self.i, self.j) == (x + 1, y - 1) and grid[x + 1][y].block and grid[x][y - 1].block:
+            return True
+
+        elif (self.i, self.j) == (x - 1, y - 1) and grid[x - 1][y].block and grid[x][y - 1].block:
+            return True
+
+        return False
 
 
 cols = 50
@@ -149,34 +79,25 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 grey = (220, 220, 220)
-yellow = (255,255,0)
+yellow = (255, 255, 0)
 light_black = (70, 70, 70)
+light_blue = (0, 255, 255)
+black = (0, 0, 0)
 w = WIDTH // cols
 h = HEIGHT // row
-cameFrom = []
 
 # create 2d array
 grid = [[0 for i in range(cols)] for i in range(row)]
 
 # Create Spots
-
+# create node for each coordinate
+# SHOW RECT
 for i in range(cols):
     for j in range(row):
-        # create node for each coordinate
         grid[i][j] = spot(i, j)
-        # SHOW RECT
-        grid[i][j].show((0, 0, 0), 1)
+        grid[i][j].show(black, 1)
 
-# Set start and end node
-# start = grid[12][5]
-# end = grid[3][6]
-
-
-# for i in range(cols):
-#     for j in range(row):
-            #grid[i][j].show((0, 0, 0), 1)
-
-
+# to create frame around screen
 for i in range(0, row):
     grid[0][i].show(grey, 0)
     grid[0][i].block = True
@@ -189,6 +110,7 @@ for i in range(0, row):
 
 
 def onsubmit():
+    # make start and end node global
     global start
     global end
     st = startBox.get().split(',')
@@ -206,7 +128,6 @@ label1 = Label(window, text='End(x,y): ')
 endBox = Entry(window)
 var = IntVar()
 showPath = ttk.Checkbutton(window, text='Show Steps :', onvalue=1, offvalue=0, variable=var)
-
 submit = Button(window, text='Submit', command=onsubmit)
 
 showPath.grid(columnspan=2, row=2)
@@ -220,7 +141,6 @@ window.update()
 mainloop()
 
 pygame.init()
-openSet.append(start)
 
 
 def mousePress(x):
@@ -235,6 +155,9 @@ def mousePress(x):
             acess.show(light_black, 0)
 
 
+# push start node to a list
+openSet.append([float('inf'), start.i, start.j])
+start.seen = True
 end.show((255, 8, 127), 0)
 start.show((255, 8, 127), 0)
 
@@ -256,89 +179,115 @@ while loop:
                 loop = False
                 break
 
-for i in range(cols):
-    for j in range(row):
-        grid[i][j].addNeighbors(grid)
-
 
 def heurisitic(n, e):
     d = math.sqrt((n.i - e.i) ** 2 + (n.j - e.j) ** 2)
-    # d = abs(n.i - e.i) + abs(n.j - e.j)
     return d
 
-light_blue = (0, 255, 255)
-surroundings = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
 
-def main():
+def Astar_finder():
+    start_time = time.time()
+    surroundings = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
 
-    if len(openSet) > 0:
-        lowestIndex = 0
-        for i in range(len(openSet)):
-            if openSet[i].f < openSet[lowestIndex].f:
-                lowestIndex = i
+    while openSet:
 
-        current = openSet[lowestIndex]
-        if current == end:
-            print('done', current.f)
-            start.show((255, 8, 127), 0)
-            temp = current.f
-            for i in range(round(current.f)):
-                current.closed = False
-                current.draw_line(yellow, 1)
-                current = current.previous
-
-            #start.draw_line(yellow,1)
-            end.show((255, 8, 127), 0)
-
-            Tk().wm_withdraw()
-            result = messagebox.askokcancel('Program Finished', (
-                        'The program finished, the shortest distance \n to the path is ' + str(
-                    temp) + ' blocks away, \n would you like to re run the program?'))
-            if result == True:
-                os.execl(sys.executable, sys.executable, *sys.argv)
-            else:
-                ag = True
-                while ag:
-                    ev = pygame.event.get()
-                    for event in ev:
-                        if event.type == pygame.KEYDOWN:
-                            ag = False
-                            break
-            pygame.quit()
-
-        openSet.pop(lowestIndex)
+        smallest_f, row, col = heapq.heappop(openSet)
+        current = grid[row][col]
         closedSet.append(current)
 
-        neighbors = current.neighbors
-        for i in range(len(neighbors)):
-            neighbor = neighbors[i]
-            if neighbor not in closedSet:
-                #g = the movement cost to move from the starting point to a given square on the grid, following the path generated to get there.
-                tempG = current.g + 1
-                if neighbor in openSet:
-                    # important cuz when seeing bigger g, just break
-                    if neighbor.g > tempG:
-                        neighbor.g = tempG
-                else:
-                    neighbor.g = tempG
-                    openSet.append(neighbor)
+        for sur in surroundings:
+            r = sur[0] + current.i
+            c = sur[1] + current.j
+            neighbor = grid[r][c]
 
-            # h = the estimated movement cost to move from that given square on the grid to the final destination.
-            neighbor.h = heurisitic(neighbor, end)
-            neighbor.f = neighbor.g + neighbor.h
+            # avoid checking duplicates
+            if not neighbor.visited:
+                neighbor.visited = True
+                # g = the movement cost to move from the starting point to a given square on the grid, following the path generated to get there.
+                neighbor.g = max(neighbor.g, current.g + 1)
+                neighbor.h = heurisitic(neighbor, end)
+                # ‘f’ which is a parameter equal to the sum of two other parameters – ‘g’ and ‘h’
+                neighbor.f = neighbor.g + neighbor.h
+                current.neighbors.add((r, c))
+                if neighbor == end:
+                    # end's node points to current
+                    neighbor.previous = current
 
-            if neighbor.previous == None:
-                neighbor.previous = current
+                    current.show(light_blue, 0)
+                    print("--- %s seconds ---" % (time.time() - start_time))
+                    get_steps(neighbor, start_time)
 
-    #if var.get():
-    for i in range(len(openSet)):
-        openSet[i].show(green, 0)
+                # check if diagonally walkable
+                if not neighbor.block:
 
-    for i in range(len(closedSet)):
-        if closedSet[i] != start:
-            closedSet[i].show((0,255,250), 0)
+                    # if diagonal move is not valid, ignore it and move on
+                    if neighbor.is_diagonally_walkable(row, col):
+                        continue
 
-    current.closed = True
+                    else:
+                        heapq.heappush(openSet, [neighbor.f, neighbor.i, neighbor.j])
+
+                    neighbor.previous = current
+
+        # draw coordinates that are not visited
+        for op in openSet:
+            cur = grid[op[1]][op[2]]
+            if not cur.opened:
+                cur.opened = True
+                cur.show_open_and_closed(green, 0)
+
+        # draw coordinates that are visited
+        for cl in closedSet:
+            if cl != start and not cl.closed:
+                cl.closed = True
+                cl.show_open_and_closed(light_blue, 0)
+
+    # when there is no path from start point to end point
+    return []
+
+
+def No_path():
+    Tk().wm_withdraw()
+    result = messagebox.askokcancel('Program Finished \n', 'There was no path from start point to end point :(')
+    if result == True:
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
+        ag = True
+        while ag:
+            ev = pygame.event.get()
+            for event in ev:
+                if event.type == pygame.KEYDOWN:
+                    ag = False
+                    break
+    pygame.quit()
+
+
+def get_steps(current, start_time):
+    # print('done', current.f)
+    start.show((255, 8, 127), 0)
+    temp = current.f
+    for i in range(round(temp)):
+        current.closed = False
+        current.draw_line(yellow, 1)
+        current = current.previous
+
+    Tk().wm_withdraw()
+    result = messagebox.askokcancel('Program Finished', (
+            'The program finished, the shortest distance \n to the path is ' + str(
+        temp) + ' blocks away, \n This program took ' + str(
+        round((time.time() - start_time), 2)) + 'seconds. \n would you like to re run the program?'))
+
+    if result == True:
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
+        ag = True
+        while ag:
+            ev = pygame.event.get()
+            for event in ev:
+                if event.type == pygame.KEYDOWN:
+                    ag = False
+                    break
+    pygame.quit()
 
 
 while True:
@@ -346,4 +295,5 @@ while True:
     if ev.type == pygame.QUIT:
         pygame.quit()
     pygame.display.update()
-    main()
+    Astar_finder()
+
